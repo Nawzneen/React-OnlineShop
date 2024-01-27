60d36fe28b4e3dcd5dd27c17977ca6be148306ce
Nazanin <earworm90@gmail.com>
2024-01-27 21:29:10 +0200

    fix details

--- src/pages/Vans/VanDetails.js
+++ src/pages/Vans/VanDetails.js
@@ -50,29 +50,30 @@ export default function VanDetails(props) {
   }, [params.id]);
 
   return (
-    <div className="vanDetails-container  ">
+    <div className="  vanDetails-container  flex-grow-1 pt-4 ">
       <Link
         to="/vans"
         aria-label={`Going back to the Vans page`}
-        className="back-btn   "
+        className="back-btn  "
       >
-        <span className="p-1 mt-4">
+        <span className="ms-4 p-2">
           <ArrowLeft />
         </span>
         Back to all vans
       </Link>
       {van ? (
-        <div className="container mt-4 mb-5">
-          <div>
+        <div className="vanDetail-card    mt-4 mb-5">
+          <div style={{ display: "grid", placeItems: "center" }}>
             <img
               src={van.imageUrl}
               alt={van.name}
-              width={`300px`}
-              className="mt-4"
+              // style={{ maxWidth: "500px", margin: "auto" }}
+              style={{ width: "100%" }}
+              className="mt-4 mb-4"
             />
           </div>
           <TypeBtn van={van} />
-          <h2 className="vanDetalis-name">{van.name}</h2>
+          <h2 className="vanDetalis-name mt-4">{van.name}</h2>
           <span className="vanDetial-price">${van.price}/day</span>
           <p className="vanDetails-description">{van.description}</p>
           <button>Rent this Van</button>
