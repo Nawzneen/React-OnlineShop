36e3752f703fb7d044b7238805befe9ae7edbde6
Nazanin <earworm90@gmail.com>
2024-02-05 21:53:58 +0200

    update apis file

--- src/Components/apis.js
+++ src/Components/apis.js
@@ -1,24 +1,6 @@
-// export async function getVans() {
-//   const res = await fetch("/api/vans");
-//   if (!res.ok) {
-//     console.log("res ius", res);
-//     //   throw {
-//     //     message: "Failed to fetch vans",
-//     //     statusText: res.statusText,
-//     //     status: res.status,
-//     //     originalError: res,
-//     //   };
-//     const error = new Error("Failed to fetch vans");
-//     error.statusText = res.statusText;
-//     error.status = res.status;
-
-//     throw error;
-//   }
-//   const data = await res.json();
-//   return data.vans;
-// }
-export async function getVans() {
-  const res = await fetch("/api/vans");
+export async function getVans(id) {
+  const url = id ? `/api/vans/${id}` : "/api/vans";
+  const res = await fetch(url);
 
   if (!res.ok) {
     console.log("res is", res);
@@ -32,21 +14,27 @@ export async function getVans() {
 
   const data = await res.json();
 
-  // if (data && data.error) {
-  //   // If the server response contains an error field, throw an error with the server's error message
-  //   const serverError = new Error(`Server Error: ${data.error}`);
-  //   serverError.responseData = data; // Attach the actual response for debugging
-  //   throw serverError;
+  // if (!data || !data.vans) {
+  //   throw {
+  //     message: "Failed to fetch vans",
+  //     statusText: res.statusText,
+  //     status: res.status,
+  //     originalRes: res,
+  //   };
   // }
 
-  if (!data || !data.vans) {
+  return data.vans;
+}
+export async function getHostVans(id) {
+  const url = id ? `/api/host/vans/${id}` : "/api/host/vans";
+  const res = await fetch(url);
+  if (!res.ok) {
     throw {
       message: "Failed to fetch vans",
       statusText: res.statusText,
       status: res.status,
-      originalRes: res,
     };
   }
-
+  const data = await res.json();
   return data.vans;
 }
