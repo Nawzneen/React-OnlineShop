e4717f4e659319efa8c267fc05bf9b50663c344b
nawzneen <earworm90@gmail.com>
2024-02-11 17:42:21 +0200

    add firestore as database

--- src/apis.js
+++ src/apis.js
@@ -1,3 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// we dont need real time events so we use lite
+import { getFirestore } from "firebase/firestore/lite";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxY3CxkXae7eVCKkDlRceIr2hXWLAbiRE",
+  authDomain: "reactshop-51969.firebaseapp.com",
+  projectId: "reactshop-51969",
+  storageBucket: "reactshop-51969.appspot.com",
+  messagingSenderId: "92349911843",
+  appId: "1:92349911843:web:6d88e3a3a599c025027296",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 export async function getVans(id) {
   const url = id ? `/api/vans/${id}` : "/api/vans";
   const res = await fetch(url);
